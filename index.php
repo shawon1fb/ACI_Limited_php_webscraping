c0683522f0cd18e924760b3e9addb14fa30bd783
shawon1fb <shawon0187@gmail.com>
2022-12-30 20:03:28 +0600

    all products

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 include "simple_html_dom.php";
 
-//$url = 'https://medex.com.bd/companies/2/aci-limited/brands';
-$url = 'data.html';
+$url = 'https://medex.com.bd/companies/2/aci-limited/brands';
+//$url = 'data.html';
 
 //
 //$html = file_get_html($url);
@@ -14,7 +14,7 @@ function getAllPageLink($url)
     $pages = [];
 
 
-    for ($i = 20; $i < 24; $i++) {
+    for ($i = 1; $i < 24; $i++) {
         $pages[] = "https://medex.com.bd/companies/2/aci-limited/brands?page=$i";
     }
 
@@ -34,8 +34,10 @@ function getAllPageLink($url)
 function getProductsFromUrl($url)
 {
 
+    $context = stream_context_create();
+    stream_context_set_params($context, array('user_agent' => 'PostmanRuntime/7.30.0'));
 //    $html = file_get_html('data.html');
-    $html = file_get_html($url);
+    $html = file_get_html($url,0, $context);
 
     $divs = $html->find("div[class='col-xs-12 col-sm-6 col-lg-4']");
 
@@ -81,7 +83,7 @@ function getProductsFromUrl($url)
     return $products;
 }
 
-echo json_encode(getProductsFromUrl($url));
+//echo json_encode(getProductsFromUrl($url));
 
 //echo "hello";
 
@@ -93,22 +95,30 @@ function parseAllProducts($url)
     $links = getAllPageLink($url);
 
 //    array_pop($links);
-    echo json_encode($links);
+    echo json_encode($links, 128);
+    echo "\n";
+    echo "\n";
 //    $temp_products = getProductsFromUrl($url);
 //    $products = array_merge($products, $temp_products);
 
+
     foreach ($links as $key => $link) {
-        echo $link;
+
+        echo "$link" . PHP_EOL;
+        sleep(5);
         $temp = getProductsFromUrl($link);
 //        echo json_encode($temp);
         file_put_contents("output_$key.json", json_encode($temp));
-//         $products = array_merge($products, $temp);
+        echo count($temp) . PHP_EOL;
+        $products = array_merge($products, $temp);
     }
+
+    file_put_contents("output-all-products.json", json_encode($products));
     return $products;
 }
 
 //getProductsFromUrl($url);
-//parseAllProducts($url);
+parseAllProducts($url);
 //parseAllProducts($url);
 //$output = json_encode(parseAllProducts($url));
 //file_put_contents('output.json', $output);
